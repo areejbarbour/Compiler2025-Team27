aabfa3865f708eb87ccfd5256d2b7b4959190705
areejbarbour <gsy0997@gmail.com>
2026-01-04 00:32:06 +0300

    Last Edit By Areej

--- src/app/WebMain.java
+++ src/app/WebMain.java
@@ -14,9 +14,9 @@ public class WebMain {
     public static void main(String[] args) throws Exception {
 
      String filePath = "example/web.html";
-      // String filePath = "example/add_product.html";
-      //  String filePath = "example/product_details.html";
-     //    String filePath = "example/products.html";
+//         String filePath = "example/add_product.html";
+//          String filePath = "example/product_details.html";
+//            String filePath = "example/products.html";
         CharStream input = CharStreams.fromFileName(filePath);
 
         WebTemplateLexer lexer = new WebTemplateLexer(input);
